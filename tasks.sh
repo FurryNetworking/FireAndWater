3177724160b527de854e2c265379985bb154cf95
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 13:51:09 -0800

    Fixups

--- tasks.sh
+++ tasks.sh
@@ -1,14 +1,14 @@
-while getopts u:a:f: flag
+while getopts t: flag
 do
     case "${flag}" in
         t) task_no=${OPTARG};;
     esac
 done
 
-if [ $task_no == 0 ]; then
+if [ $task_no = 0 ]; then
   echo [INFO] Cleaning anaconda script.....
   rm -rf ./delete_anaconda_script
-elif [ $task_no == 1 ]; then
+elif [ $task_no = 1 ]; then
   echo [INFO] Cleaning files that did not meet gitignore.....
   if [[ -f requirements.txt ]]; then
     rm -rf requirements.txt
@@ -19,25 +19,27 @@ elif [ $task_no == 1 ]; then
   if [[ -f anaconda.xml ]]; then
     rm -rf anaconda.xml
   fi
-elif [ $task_no == 2 ]; then
+elif [ $task_no = 2 ]; then
   sudo zypper install typescript htop
-elif [ $task_no == 3 ]; then
+elif [ $task_no = 3 ]; then
   chmod 755 Makefile*
   chmod 755 Dockerfile*
-elif [ $task_no == 4 ]; then
+elif [ $task_no = 4 ]; then
   sudo zypper install make
-elif [ $task_no == 5 ]; then
+elif [ $task_no = 5 ]; then
   make tumbleweed
   make deps_instances
-elif [ $task_no == 6 ]; then
+elif [ $task_no = 6 ]; then
   make leap
   make deps_instances
-elif [ $task_no == 8 ]; then
+elif [ $task_no = 7 ]; then
+  make ruby_installation
+elif [ $task_no = 8 ]; then
   chmod 755 *.ts
   if [[ -f configurator.ts ]]; then
     ts-node configurator.ts
   fi
-elif [ $task_no == 9 ]; then
+elif [ $task_no ==9 ]; then
   git clone https://github.com/cginternals/cppfs
   set path=$CD/cppfs
   cd $path
