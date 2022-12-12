1cfc880ab6d29805a191474af18aeaa8868a8044
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 08:31:13 -0800

    Update get_element.py

--- get_element.py
+++ get_element.py
@@ -1,5 +1,20 @@
-import lxml.etree
-# Modified from a stackoverflow.
-root = lxml.etree.fromstring(xmlstr)
-textelem = root.find('config/version')
-print textelem.text
+from lxml import etree as et
+import logging
+import logging.handlers
+
+with open('data.txt', 'r') as file:
+    data = file.read()
+
+root = et.fromstring(data)
+
+data_type = root.xpath('//root/version/@type')[0]
+data_type = root.find('version').get('type')
+
+if data_type == "integer_data":
+  my_logger = logging.getLogger('MyLogger')
+  my_logger.setLevel(logging.DEBUG)
+  handler = logging.handlers.SysLogHandler(address = '/dev/log')
+  my_logger.addHandler(handler)
+  my_logger.debug('[INFO: get_element.py] Check has passed!')
+  textelem = root.find('root/version')
+  print textelem.text
