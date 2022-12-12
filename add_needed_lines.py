8e663e03ad877f88a2b8a198e10e8836e8058b61
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 11:53:54 -0800

    Fix filenames

--- add_needed_lines.py
+++ add_needed_lines.py
@@ -1,16 +1,16 @@
 with open('requirements.txt', 'r') as file:
     L = file.read().replace('\n', '')
-file1 = open("myfile.txt", "w")
+file1 = open("requirements.txt", "w")
 file1.writelines(L)
 file1.close()
-file1 = open("myfile.txt", "a")
+file1 = open("requirements.txt", "a")
 data_to_write = [
   "\n",
   "logging",
 ]
 for line in data_to_write:
   file1.write(line)
-file1 = open("myfile.txt", "r")
+file1 = open("requirements.txt", "r")
 print("Output of Readlines after appending")
 print(file1.read())
 print()
