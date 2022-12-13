f294b5cc7b780ad58c7f1b4e3068a9e323427fae
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 16:38:50 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
+npm i --save-dev @types/node
 OUTPUT=$(tsc get_anaconda_version.ts)
 wget https://repo.anaconda.com/archive/Anaconda$OUTPUT-Linux-x86_64.sh
 sudo bash Anaconda$OUTPUT-Linux-x86_64.sh
