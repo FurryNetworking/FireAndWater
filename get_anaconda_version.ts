d66a0f0f5691b01f1ef660c0f6233f1862806657
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 17:05:29 -0800

    Update get_anaconda_version.ts

--- get_anaconda_version.ts
+++ get_anaconda_version.ts
@@ -1,7 +1,19 @@
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
+var xpath = require("xml2js-xpath");
+ 
 import { readFileSync } from 'fs';
+const options = {
+        explicitArray: false,
+        ignoreAttrs: true
+    };
 const file = readFileSync('./anaconda.xml', 'utf-8');
-parser.parseString(file, function (err, result) {
-    console.log(result['root']['data']['version']);
+xml2js.parseString(file, options, function(err, json) {
+
+  // find all elements: returns xml2js JSON of the element
+  var matches = xpath.find(json, "//root");
+  console.log(matches[0]['version'])
+  // find the first element, and get its id:
+  var matches = xpath.evalFirst(json, "//root//", "version");
+  //console.log(matches.fname)
 });
