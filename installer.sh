dd9a03a61efe58f160185caf9ccd69ab76811664
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 16:42:57 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 npm i --save-dev @types/node
-OUTPUT=$(tsc get_anaconda_version.ts)
+tsc get_anaconda_version.ts
+OUTPUT=$(node get_anaconda_version.js)
 wget https://repo.anaconda.com/archive/Anaconda$OUTPUT-Linux-x86_64.sh
 sudo bash Anaconda$OUTPUT-Linux-x86_64.sh
