4d1d4c9779d8d1c5d1dfefd09f1a71252ec90252
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 17:06:21 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,7 @@
 npm i --save-dev @types/node
 tsc get_anaconda_version.ts
 npm install xml2js
+npm install xml2js-xpath
 OUTPUT=$(node get_anaconda_version.js)
 wget https://repo.anaconda.com/archive/Anaconda$OUTPUT-Linux-x86_64.sh
 sudo bash Anaconda$OUTPUT-Linux-x86_64.sh
