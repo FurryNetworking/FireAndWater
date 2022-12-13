c4b2c49feafe3370b04e9caafaf7510008992d02
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 16:39:57 -0800

    Update get_anaconda_version.ts

--- get_anaconda_version.ts
+++ get_anaconda_version.ts
@@ -2,6 +2,6 @@ var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 import { readFileSync } from 'fs';
 const file = readFileSync('./anaconda.xml', 'utf-8');
-parser.parseString(xml, function (err, result) {
+parser.parseString(file, function (err, result) {
     console.log(result['data']['version']);
 });
