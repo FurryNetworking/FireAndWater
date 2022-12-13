a644056f753a93d51cccca8ef9e68a28fe78ca95
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-12-12 17:31:15 -0800

    Update executor.rb

--- executor.rb
+++ executor.rb
@@ -1,7 +1,7 @@
 require 'mixlib/shellout'
 first = ["2e"].pack('H*')
 last = ["2f"].pack('H*')
-0.step(9, 1) do |n|
+0.step(8, 1) do |n|
   cmd = first + last + "tasks.sh -t #{n.to_s.delete(' ')}"
   so = Mixlib::ShellOut.new(cmd)
   so.live_stream = $stdout
