6a28c6f3d5f5ea3f1717b3cd1e879cf378901b2b
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2023-02-19 11:53:59 -0700

    Use new engine

--- installer.sh
+++ installer.sh
@@ -5,6 +5,6 @@ npm install xml2js-xpath
 npm install -g typescript
 npm install -g ts-node
 ts-node configurator.ts
-OUTPUT=$(node get_anaconda_version.js)
+OUTPUT=$(ts-node get_anaconda_version.ts)
 wget https://repo.anaconda.com/archive/Anaconda$OUTPUT-Linux-x86_64.sh
 sudo bash Anaconda$OUTPUT-Linux-x86_64.sh
