0a430949c7463c23c88cb73c6d8d39659dfac688
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2023-02-19 12:22:13 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,6 +6,10 @@ npm install -g typescript
 npm install -g ts-node
 ts-node configurator.ts
 zypper -n install python-base
+zypper addrepo https://download.opensuse.org/repositories/devel:languages:python/openSUSE_Tumbleweed/devel:languages:python.repo
+zypper refresh
+zypper install python2-setuptools
+zypper install python2-pip
 pip install -r requirements.txt
 OUTPUT=$(python2 get_version.py)
 wget https://repo.anaconda.com/archive/Anaconda$OUTPUT-Linux-x86_64.sh
