589795e45066870c6490a5973e15935f1881d9f7
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2023-02-19 13:42:05 -0700

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,4 +1,7 @@
 zypper addrepo https://download.opensuse.org/repositories/Virtualization:containers/openSUSE_Tumbleweed_and_d_l_g/Virtualization:containers.repo
 sudo zypper ar -cfp 90 https://ftp.fau.de/packman/suse/openSUSE_Tumbleweed/ packman
+zypper addrepo https://download.opensuse.org/repositories/Base:System/standard/Base:System.repo
+zypper refresh
+zypper install glibc
 zypper refresh
 zypper install docker-compose
