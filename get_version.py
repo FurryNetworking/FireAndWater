b87f88b3b9378bb2f86645a85b6d3c152c838fb1
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2023-02-19 12:01:26 -0700

    Fix indents

--- get_version.py
+++ get_version.py
@@ -2,19 +2,19 @@ from lxml import etree as et
 import logging
 import logging.handlers
 
- with open('anaconda.xml', 'r') as file:
-     data = file.read()
+with open('anaconda.xml', 'r') as file:
+ data = file.read()
 
- root = et.fromstring(data)
+root = et.fromstring(data)
 
- data_type = root.xpath('//root/version/@type')[0]
- data_type = root.find('version').get('type')
+data_type = root.xpath('//root/version/@type')[0]
+data_type = root.find('version').get('type')
 
- if data_type == "integer_data":
-   my_logger = logging.getLogger('MyLogger')
-   my_logger.setLevel(logging.DEBUG)
-   handler = logging.handlers.SysLogHandler(address = '/dev/log')
-   my_logger.addHandler(handler)
-   my_logger.debug('[INFO: get_version.py] Check has passed!')
-   textelem = root.find('root/version')
-   print textelem.text
+if data_type == "integer_data":
+ my_logger = logging.getLogger('MyLogger')
+ my_logger.setLevel(logging.DEBUG)
+ handler = logging.handlers.SysLogHandler(address = '/dev/log')
+ my_logger.addHandler(handler)
+ my_logger.debug('[INFO: get_version.py] Check has passed!')
+ textelem = root.find('root/version')
+ print textelem.text
